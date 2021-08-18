9e9e0b6a3f42179da3906ab645b2408ba28dfdbc
jocsanex5 <jocsanex5@gmail.com>
2021-08-18 07:22:14 -0600

    actualizando fuentes

--- main.js
+++ main.js
@@ -5,11 +5,16 @@
     const load = () =>{
         
         let loader = document.getElementById('loader');
-        
-        window.addEventListener('load', ()=>{
-            
-            loader.style.display = 'none';
-        });
+
+        let cont=0, time = setInterval(()=>{ cont++;
+
+            if(cont==1){
+
+                clearInterval(time);
+                loader.style.display = 'none';
+            }
+
+        }, 3000);
     }
     
     load();
